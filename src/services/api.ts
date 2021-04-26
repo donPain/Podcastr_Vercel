b6bb5a3a90d5365893dee224f640d8ac140f922f
donPain <eduardo.donzeli@fmxsolucoes.com.br>
2021-04-26 18:35:37 -0300

    Corrigindo problema de deploy static api

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,7 @@
 import axios from 'axios';
 
-export const api = axios.create( {
-    baseURL: 'http://localhost:3333/'
+export const api = axios.create({
+  baseURL: 'http://localhost:3333/'
+  // configure connection with deployed fake api
+  baseURL: process.env.API_URL || 'http://localhost:3333/'
 })
